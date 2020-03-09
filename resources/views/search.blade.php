08acbb4cc7b2131213d6e65d0cd6672b38bda52f
Basit Ali <basitalikhatri98@gmail.com>
2020-03-09 13:01:55 +0500

    buttons styled

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -31,11 +31,20 @@
                                                                 <span>{{$searchuser->country}}</span>
 
                                                                 @if ($searchuser->family_user != null)
-                                                                <a href="{{ route('viewprofilefamily',['id'=>$searchuser->user_id,'circle_id'=>2]) }}" title="" class="add-butn more-action" style = "background-color: red" data-ripple="">Family</a>
+                                                                    @if (Auth::user()->family_user == null)
+                                                                    <a href="{{ route('viewprofilefamily',['id'=>$searchuser->user_id,'circle_id'=>2]) }}" style="pointer-events: none; cursor: default; background-color: red;" title="" class="add-butn more-action" data-ripple="">Family</a>
+                                                                    @else
+                                                                    <a href="{{ route('viewprofilefamily',['id'=>$searchuser->user_id,'circle_id'=>2]) }}" title="" class="add-butn more-action" style = "background-color: red" data-ripple="">Family</a>
+                                                                    @endif
                                                                 @endif
 
                                                                 @if ($searchuser->business_user != null)
-                                                                <a href="{{ route('viewprofilebusiness',['id'=>$searchuser->user_id,'circle_id'=>3]) }}" title="" class="add-butn" data-ripple="">Business</a>
+                                                                    @if (Auth::user()->business_user == null)
+                                                                    <a href="{{ route('viewprofilebusiness',['id'=>$searchuser->user_id,'circle_id'=>3]) }}" style="pointer-events: none; cursor: default;"  title="" class="add-butn" data-ripple="">Business</a>
+                                                                    @else
+                                                                    <a href="{{ route('viewprofilebusiness',['id'=>$searchuser->user_id,'circle_id'=>3]) }}" title="" class="add-butn" data-ripple="">Business</a>
+                                                                    @endif
+
                                                                 @endif
 
                                                             </div>
