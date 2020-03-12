2092e5598472e70a17829d1638d92f016bf2e9f6
Basit Ali <basitalikhatri98@gmail.com>
2020-03-12 12:55:56 +0500

    factory and seeds

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,10 +9,13 @@ use Carbon\Carbon;
 $factory->define(Post::class, function (Faker $faker) {
     return [
         'title' =>$faker->realText($maxNbChars = 10, $indexSize = 2),
-        'content' => $faker->realText($maxNbChars = 100, $indexSize = 2) ,
+        'content' => $faker->sentence() ,
         'user_id' => $faker->numberBetween($min = 1, $max = 50),
         'circle_id' => $faker->numberBetween($min = 1, $max = 3),
         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        // 'picture' => $faker->imageUrl(640, 480, 'cats', true, 'Faker'),
+        // 'picture' => $faker->imageUrl($width = 640, $height = 480),
+        // 'picture'=> $faker->image($dir = public_path('images/posts/Friend'), $width = 640, $height = 480),
         // 'picture'=>'https://source.unsplash.com/random',
     ];
 });
