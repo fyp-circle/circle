744fe731e51a8d45bfa1400455df8de8fb6f0d68
Munir Hassan <k163761@nu.edu.pk>
2020-06-06 00:08:35 +0500

    loader final

--- resources/views/layouts/main_lay.blade.php
+++ resources/views/layouts/main_lay.blade.php
@@ -481,7 +481,6 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="{{asset('/js/message.js')}}"></script>
-    
     <script>
         $(document).ready(function () {
             //Preloader
